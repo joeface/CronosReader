855a8990a867a80ae3cdb080386597c168350311
Mikhail Ageev <info@michaelageev.com>
2021-09-23 11:56:56 +0200

    Surname indexer

--- parse.py
+++ parse.py
@@ -1,4 +1,6 @@
 import os
+import re
+import json
 from io import StringIO, BytesIO
 from bs4 import BeautifulSoup, SoupStrainer
 from lxml import etree
@@ -6,8 +8,16 @@ from lxml import etree
 # Tuple with relative filenames to parse
 FILES = ('full.html',)
 
+# Global record storage
+RECORDS = []
+
+# Global surnames storage
+SURNAMES = []
+
 
 def read_file(file_name):
+
+    global RECORDS
 
     if os.path.exists(file_name):
         try:
@@ -26,7 +36,6 @@ def read_file(file_name):
             StringIO(file_content), parser)
 
         tables = []
-        records = []
 
         counter_date = 0
         counter_doc = 0
@@ -76,28 +85,72 @@ def read_file(file_name):
                                             is_doc = True
                                             counter_doc += 1
 
-                                        # print(txt)
                     if rec:
-                        records.append(rec)
+                        RECORDS.append(rec)
                         rec = None
                         is_date = False
                         is_doc = False
-
-        # print('Reading completed. Number of tables: %d, records: %d, dates: %d docs: %d' %
-        #      (len(tables), len(records), counter_date, counter_doc))
-
-        for r in records:
-            print(r['date'])
-            print(r['doc'], '\n\n')
 
     else:
         print('File %s not found' % (file_name))
 
 
-def run():
+def parse():
+
+    global RECORDS
+
+    # Read data from files
     for f in FILES:
         read_file(f)
 
+    # Output records
+    for rec in RECORDS:
+        print(rec['date'])
+        print(replace_names(rec['doc']), '\n\n')
+
+
+def search_names():
+    try:
+        with open('dump.txt', 'r', encoding='utf-8') as dump:
+            file_content = dump.read()
+    except:
+        print('! Error reading dump file')
+        return False
+
+    names = re.findall(
+        r'([а-я]+ [а-я]{1}\.[а-я]{1}\.)[\W]+', file_content, re.I)
+
+    for n in names:
+        print(n)
+
+    print('Total number of names found: %d' % (len(names)))
+
+
+def index_surnames():
+
+    global SURNAMES
+
+    try:
+        with open('surnames.json') as json_file:
+            file_content = json.load(json_file)
+    except:
+        print('! Error reading surnames file')
+        return False
+
+    min_len = 20
+
+    for surname in file_content:
+        SURNAMES.append(surname['Surname'])
+        if len(surname['Surname']) < min_len:
+            min_len = len(surname['Surname'])
+
+    print('Total number of surnames found: %d' % (len(SURNAMES)))
+    print('Mininal surname length: %d' % (min_len))
+
+
+def replace_names(text):
+    return re.sub(r'([а-я]+ [а-я]{1}\.[а-я]{1}\.)[\W]+', '--- ', text, flags=re.I)
+
 
 if __name__ == '__main__':
-    run()
+    index_surnames()
